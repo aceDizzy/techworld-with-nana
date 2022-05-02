9bed3a36a6cf95c69f8de6388a6c569ffd77db48
Anna Clarisse T. Dela Cruz <93988552+aceDizzy@users.noreply.github.com>
2022-05-02 09:51:13 +0800

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,45 @@
+my_set = {"January", "February", "March"}
+for element in my_set:
+    print(element)
+
+my_set.add("April")
+print(my_set)
+my_set.remove("January")
+print(my_set)
+
+my_list = ["January", "February","March","January"]
+my_list.remove("January")
+print(my_list)
+
+
+
+
+# Standalone built-in functions
+print("Some text")
+input("Enter value")
+set([1,2,3])
+int("20")
+
+"2,3".split()
+print("test".capitalize())
+[1,2,3].count()
+
+my_list = ["20", "30", "100"]
+print(my_list[2])
+
+my_dictionary = {"days":20, "unit": "hours", "message": "Study hard!"}
+print(my_dictionary["message"])
+
+
+
+
+# data types
+message = "enter some value" #string
+days = 20 #int
+price = 9.99 #float
+valid_number = True #boolean
+exit_input = False #boolean
+list_of_days = [20, 40, 20, 100] #lists for numbers w/ duplicate - arranged []
+list_of_months = ["January, February, March"] #lists for strings
+set_of_days = {20, 45, 100} #sets for numbers w/o duplicate - not arranged {}
+days_and_unit = {"days": 10, "unit": "hours"}
---
+++ time-till-deadline.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+user_input = input("Enter your goal with a deadline separated by colon\n")
+input_list = user_input.split(":")
+
+goal = input_list[0]
+deadline = input_list[1]
+
+#module and definition datetime
+deadline_date = datetime.datetime.strptime(deadline, "%m.%d.%Y")
+
+today_date = datetime.datetime.today()
+
+time_till = deadline_date - today_date
+
+hours_till = int(time_till.total_seconds() / 60 / 60)
+print(f"Dear User! Time remaining for your goal: {goal} is {hours_till} hours")
